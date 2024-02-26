e849fd47f8b26988204c216786df0777ae914aa3
Igor <igor.sheplyakov@yandex.ru>
2024-02-26 22:51:44 +0300

    two sum fix

--- src/main/java/ru/igor/TwoSum.java
+++ src/main/java/ru/igor/TwoSum.java
@@ -9,14 +9,17 @@ public class TwoSum {
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
         int[] result = new int[2];
+
         for (int i = 0; i < n; i++) {
-            if (map.containsKey(target - nums[i])) {
+            int number = target - nums[i];
+            if (map.containsKey(number)) {
                 result[1] = i;
-                result[0] = map.get(target - nums[i]);
+                result[0] = map.get(number);
                 return result;
             }
             map.put(nums[i], i);
         }
+
         return result;
     }
 }
