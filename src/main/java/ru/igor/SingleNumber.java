3c783d580143f72e7f7f1fcd81c4ce1faa4ba495
Igor <igor.sheplyakov@yandex.ru>
2024-02-26 14:19:07 +0300

    fix

--- src/main/java/ru/igor/SingleNumber.java
+++ src/main/java/ru/igor/SingleNumber.java
@@ -1,8 +1,8 @@
 package ru.igor;
 
 // 136
-//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-//You must implement a solution with a linear runtime complexity and use only constant extra space.
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
 
 public class SingleNumber {
     public static int singleNumber(int[] nums) {
--- src/main/java/ru/igor/ValidParentheses.java
+++ src/main/java/ru/igor/ValidParentheses.java
@@ -3,13 +3,14 @@ package ru.igor;
 import java.util.Stack;
 
 // 20
-//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
 // determine if the input string is valid.
 public class ValidParentheses {
     public static boolean validParentheses(String s) {
         var charStack = new Stack<Character>();
+        var charArray = s.toCharArray();
 
-        for (char c : s.toCharArray()) {
+        for (char c : charArray) {
             if (c == '(') {
                 charStack.push(')');
             } else if (c == '{') {
